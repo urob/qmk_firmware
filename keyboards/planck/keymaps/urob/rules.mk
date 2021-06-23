603fc2599012b6d75d49aa7475c4bbf96955fb9f
urob <roulbricht@gmail.com>
2021-06-23 14:22:17 -0400

    add comment about lto-wrapper warning

--- keyboards/planck/keymaps/urob/rules.mk
+++ keyboards/planck/keymaps/urob/rules.mk
@@ -2,7 +2,9 @@ UNICODEMAP_ENABLE = yes
 COMBO_ENABLE = yes
 
 # Enable link time optimization and turn of unused features
-LTO_ENABLE = yes
+LTO_ENABLE = yes # yields weird warning during compilation due to
+                 # GCC 10 bug. Can be ignored, see here for details:
+				 # http://www.forum.chibios.org/viewtopic.php?t=5696&start=23
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 TERMINAL_ENABLE = no
