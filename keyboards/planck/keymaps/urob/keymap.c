b45f2bc851c72ecdc886cb59b78418440e20f48a
urob <roulbricht@gmail.com>
2021-09-03 08:52:07 -0400

    switch back to standard numpad
    an experiment failed.... while I like the theory, I just couldn't get the mirrored and row-shifted numpad into my long-term muscle memory

--- keyboards/planck/keymaps/urob/keymap.c
+++ keyboards/planck/keymaps/urob/keymap.c
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NUM] = LAYOUT_planck_grid(
-    KC_ESC,  KC_9,    KC_8,    KC_7,    KC_ASTR,   _______, _______,   _______, _______, _______, _______, _______,
-    KC_ENT,  KC_3,    KC_2,    KC_1,    KC_PLUS,   _______, _______,   _______, OS_SHFT, OS_CTRL, OS_ALT , OS_GUI,
-    KC_TAB,  KC_6,    KC_5,    KC_4,    KC_SLSH,   _______, _______,   _______, _______, _______, _______, _______,
+    KC_ESC,  KC_7,    KC_8,    KC_9,    KC_ASTR,   _______, _______,   _______, _______, _______, _______, _______,
+    KC_ENT,  KC_4,    KC_5,    KC_6,    KC_PLUS,   _______, _______,   _______, OS_SHFT, OS_CTRL, OS_ALT , OS_GUI,
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_SLSH,   _______, _______,   _______, _______, _______, _______, _______,
     _______, _______, KC_DOT,  KC_0,    KC_MINS,   _______, _______,   _______, _______, _______, _______, _______
 ),
 
 [_FN] = LAYOUT_planck_grid(
-    KC_F12 , KC_F9  , KC_F8  , KC_F7  , _______,   _______, _______,   _______, _______, _______, _______, _______,
-    KC_F11 , KC_F3  , KC_F2  , KC_F1  , _______,   _______, _______,   _______, OS_SHFT, OS_CTRL, OS_ALT , OS_GUI,
-    KC_F10 , KC_F6  , KC_F5  , KC_F4  , _______,   _______, _______,   _______, _______, _______, _______, _______,
+    KC_F12 , KC_F7  , KC_F8  , KC_F9  , _______,   _______, _______,   _______, _______, _______, _______, _______,
+    KC_F11 , KC_F4  , KC_F5  , KC_F6  , _______,   _______, _______,   _______, OS_SHFT, OS_CTRL, OS_ALT , OS_GUI,
+    KC_F10 , KC_F1  , KC_F2  , KC_F3  , _______,   _______, _______,   _______, _______, _______, _______, _______,
     _______, _______, KC_ESC , KC_SPC , KC_TAB ,   _______, _______,   _______, _______, _______, _______, _______
 ),
 
@@ -157,7 +157,7 @@ void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
         }
     } else { /* repeat action defined here */
         // first check whether last keycode was shift or enter, or we paused for a while. It so morth to OS shift
-         if ((last_keycode == KC_SPC) || (last_keycode == KC_ENT) || (timer_elapsed(non_combo_input_timer) > 400)) {
+         if ((last_keycode == KC_SPC) || (last_keycode == KC_ENT) || (last_keycode == KC_BSPC) || (timer_elapsed(non_combo_input_timer) > 400)) {
             if (record->event.pressed) {
                add_oneshot_mods(MOD_BIT(KC_LSFT));
             }
